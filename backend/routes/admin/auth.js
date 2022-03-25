e62556b6d100d06da17a444964fff7ec07015404
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-03-25 17:10:31 +0530

    Refactor folder structure

--- backend/routes/admin/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const Router = express.Router();
-
-/* ----------------- Routes ----------------- */
-Router.post('/login', require('../../controllers/admin/login'));
-
-module.exports = Router;
--- backend/routes/client/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const Router = express.Router();
-
-/* -------------- Routes ------------------ */
-Router.post('/register', require('../../controllers/client/register'));
-
-Router.post('/login', require('../../controllers/client/login'));
-
-module.exports = Router;
