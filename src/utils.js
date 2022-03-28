f7b933a8481af4ee97cc2aeac0b667e4b88e0b3c
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-03-28 21:49:53 +0530

    Add utility functions for formatting file name and file size

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
-import { toast } from 'react-toastify';
+import { Slide, toast } from 'react-toastify';
 
 export const toastify = (message, type = 'success', theme = 'dark') =>
   toast(message, {
-    position: 'bottom-center',
+    position: 'bottom-right',
     theme,
     type,
     autoClose: 1000,
@@ -10,4 +10,35 @@ export const toastify = (message, type = 'success', theme = 'dark') =>
     closeOnClick: true,
     pauseOnHover: true,
     draggable: true,
+    transition: Slide,
   });
+
+// **************** Formatting The Size Of The File **************** //
+export function formatBytes(bytes) {
+  if (bytes === 0) return '0 Bytes';
+
+  const kbToByte = 1024;
+  const sizes = ['Bytes', 'KB', 'MB'];
+  const size = Math.floor(Math.log(bytes) / Math.log(kbToByte));
+
+  return (
+    parseFloat((bytes / Math.pow(kbToByte, size)).toFixed(2)) +
+    ' ' +
+    sizes[size]
+  );
+}
+
+// **************** Compress The File Name **************** //
+export function getSmallerFileName(filename) {
+  if (filename.length > 20) {
+    const length = filename.length;
+    return `${filename.slice(0, 20)}...${filename.slice(length - 10, length)}`;
+  }
+  return filename;
+}
+
+export const setTokenInLocalStorage = (key, token) =>
+  localStorage.setItem(key, JSON.stringify(token));
+
+export const getTokenFromLocalstorage = () =>
+  JSON.parse(localStorage.getItem('uAccessToken') || '');
