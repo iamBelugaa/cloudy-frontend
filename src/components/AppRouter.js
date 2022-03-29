5a2ea0ca313145f48fa44e42f3b50d263224be90
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-03-29 10:19:49 +0530

    Add new routes

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
+  Redirect,
   Route,
   Switch,
-  Redirect,
   useLocation,
 } from 'react-router-dom';
 import FourOhFourPage from '../pages/404Page';
-import DashboardPage from '../pages/Dashboard';
-import LoginPage from '../pages/LoginPage';
 import AdminLoginPage from '../pages/AdminLoginPage';
-import SignUp from '../pages/SignUpPage';
+import DashboardPage from '../pages/Dashboard';
+import FilesPage from '../pages/FilesPage';
 import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
+import ProfilePage from '../pages/ProfilePage';
+import SignUp from '../pages/SignUpPage';
+import UploadPage from '../pages/UploadPage';
 
 function App() {
   return (
@@ -22,6 +25,9 @@ function App() {
         <ProtectedRoute path="/login" exact component={LoginPage} />
         <ProtectedRoute path="/admin/login" exact component={AdminLoginPage} />
         <ProtectedRoute path="/dashboard" exact component={DashboardPage} />
+        <ProtectedRoute path="/upload" exact component={UploadPage} />
+        <ProtectedRoute path="/files" exact component={FilesPage} />
+        <ProtectedRoute path="/profile" exact component={ProfilePage} />
         <Route path="/*" component={FourOhFourPage} />
       </Switch>
     </Router>
