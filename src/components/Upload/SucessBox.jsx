add15f5a6fa8fd324990324078d9f75c079ae212
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-04-02 00:37:35 +0530

    Add success compoent to show gentle message after finish uploading

---
+++ src/components/Upload/SucessBox.jsx
@@ -0,0 +1,171 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { COLORS } from '../styles/ColorStyles';
+import { Caption, H2 } from '../styles/TextStyles';
+import copyIcon from '../../assets/icons/copy.svg';
+import config from '../../config.json';
+import { toastify } from '../../utils';
+
+const SucessBox = ({ uid, sendMail, resetState }) => {
+  const [email, setEmail] = useState('');
+
+  return (
+    <Wrapper>
+      <H2 style={{ fontWeight: 600, textAlign: 'center' }}>
+        File uploaded{' '}
+        <span
+          style={{
+            fontSize: '55px',
+            color: COLORS.secondary1,
+            textShadow: '0px 20px 40px rgba(23, 0, 102, 0.2)',
+          }}
+        >
+          successfully.
+        </span>
+      </H2>
+      <ActionContainer>
+        <div>
+          <Caption>Share Link</Caption>
+          <input
+            type="text"
+            value={`${config.rootDomain}/download/${uid}`}
+            readOnly
+          />
+          <Icon
+            src={copyIcon}
+            alt="Copy Icon"
+            onClick={() => {
+              navigator.clipboard.writeText(
+                `${config.rootDomain}/download/${uid}`
+              );
+              toastify('Link copied.');
+            }}
+          />
+        </div>
+        <div>
+          <Caption>Or send via Mail</Caption>
+          <input
+            type="email"
+            value={email}
+            placeholder="Reciever's email..."
+            onChange={(e) => setEmail(e.target.value.trim())}
+          />
+        </div>
+        <Button type="button" onClick={() => sendMail(email)}>
+          Send Mail
+        </Button>
+      </ActionContainer>
+      <Button type="button" onClick={() => resetState()}>
+        Upload Another
+      </Button>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 500px;
+  border-radius: 30px;
+  padding: 40px 20px 30px;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
+    0px 20px 40px rgba(23, 0, 102, 0.2),
+    inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
+  display: grid;
+  justify-content: center;
+  grid-template-columns: auto;
+  gap: 45px;
+  color: ${COLORS.text3};
+`;
+
+const Icon = styled.img`
+  position: absolute;
+  width: 20px;
+  right: 12px;
+  top: 37px;
+  cursor: pointer;
+`;
+
+const ActionContainer = styled.div`
+  position: relative;
+  justify-self: center;
+  text-align: center;
+  display: grid;
+  gap: 30px;
+  position: relative;
+
+  ::after {
+    content: 'OR';
+    position: absolute;
+    width: 100%;
+    bottom: -30px;
+    left: 0;
+  }
+
+  div {
+    display: grid;
+    gap: 10px;
+  }
+
+  input {
+    font-family: inherit;
+    font-weight: 600;
+    font-size: 15px;
+    color: inherit;
+    width: 360px;
+    height: 30px;
+    text-align: center;
+    padding: 5px 20px;
+    border: none;
+    outline: none;
+    background: none;
+    border-radius: 30px;
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
+      0px 20px 40px rgba(23, 0, 102, 0.2),
+      inset 0px 0px 0px 0.7px rgba(255, 255, 255, 0.5);
+
+    :first-of-type {
+      padding-right: 40px;
+    }
+
+    :focus {
+      box-shadow: rgb(47 184 255 / 30%) 0px 10px 40px,
+        rgb(47 184 255) 0px 0px 0px 1px inset;
+    }
+
+    ::placeholder {
+      font-size: 14px;
+      font-weight: 500;
+      color: ${COLORS.text4};
+    }
+  }
+`;
+
+const Button = styled.button`
+  width: 200px;
+  justify-self: center;
+  color: ${COLORS.text3};
+  cursor: pointer;
+  border: none;
+  outline: none;
+  font-weight: 500;
+  font-size: 15px;
+  padding: 10px 30px;
+  background-color: ${COLORS.primary1};
+  border-radius: 20px;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
+    0px 20px 40px rgba(23, 0, 102, 0.2);
+  transition: all 0.4s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  :hover {
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
+      0px 30px 60px rgba(23, 0, 102, 0.4);
+    transform: translateY(-3px);
+  }
+
+  :active {
+    transform: translateY(0px);
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
+      0px 25px 45px rgba(23, 0, 102, 0.4);
+  }
+`;
+
+export default SucessBox;
