8be4e92d3a15c191987cebefb15f90711ccaf4c3
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-04-02 00:37:05 +0530

    Add progress component to show the uploading progress

---
+++ src/components/Upload/ProgressContainer.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import { COLORS } from '../styles/ColorStyles';
+import { BodyMain, Caption, SmallText } from '../styles/TextStyles';
+
+const ProgressContainer = ({ progress }) => {
+  return (
+    <Wrapper>
+      <Progress>
+        <BodyMain>Uploading...</BodyMain>
+        <ProgressBar progress={progress} />
+        <Caption>{progress}%</Caption>
+      </Progress>
+      <SmallText style={{ color: COLORS.text4, fontWeight: 500 }}>
+        Note : Your file is currently uploading. Don't close the browser window
+        or refresh the page or file will not be uploaded.
+      </SmallText>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 450px;
+  height: 170px;
+  border-radius: 20px;
+  padding: 30px;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
+    0px 20px 40px rgba(23, 0, 102, 0.2),
+    inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
+  display: grid;
+  gap: 30px;
+  align-content: center;
+  color: ${COLORS.text3};
+`;
+
+const ProgressBar = styled.div`
+  width: ${(p) => +p.progress + '%'};
+  height: ${(p) => (p.progress ? '8px' : '0px')};
+  background-color: ${(p) => (p.progress ? COLORS.primary1 : 'none')};
+  border-radius: 10px;
+  transition: all 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
+`;
+
+const Progress = styled.div`
+  display: grid;
+  gap: 8px;
+`;
+
+export default ProgressContainer;
