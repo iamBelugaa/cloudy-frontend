9f0d0375112606aa67ce599a6b488a6ec3e2dd1a
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-04-03 11:56:40 +0530

    Refactor fetching of recent files using useFiles hook

--- src/pages/Client/Dashboard.jsx
+++ src/pages/Client/Dashboard.jsx
@@ -1,44 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import errorTrash from '../../assets/illustrations/error-trash.svg';
 import DashboardIndex from '../../components/Dashboard/DashboardIndex';
 import RecentFiles from '../../components/Dashboard/FilesIndex';
 import ShowError from '../../components/Errors/ShowError';
 import Loading from '../../components/Loading/TextLoading';
-import styled from 'styled-components';
-import { fetchRecentFiles } from '../../services/dashboardService';
-import { getTokenFromLocalstorage, paginate } from '../../utils';
 import Pagination from '../../components/shared/Pagination';
-import { SmallText } from '../../components/styles/TextStyles';
 import { COLORS } from '../../components/styles/ColorStyles';
+import { SmallText } from '../../components/styles/TextStyles';
+import { useFiles } from '../../hooks/useFiles';
+import { getTokenFromLocalstorage, paginate } from '../../utils';
 const PAGE_SIZE = 5;
 
 const Dashboard = () => {
-  const [files, setFiles] = useState(null);
-  const [error, setError] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
   const token = getTokenFromLocalstorage();
-  const history = useHistory();
+  const [files, setFiles, error] = useFiles('recent-files', token);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const paginatedFiles = paginate(files, PAGE_SIZE, currentPage);
   const handlePageChange = (page) => setCurrentPage(page);
-
-  useEffect(() => {
-    if (!token) {
-      localStorage.removeItem('uAccessToken');
-      return history.push('/login');
-    }
-
-    fetchRecentFiles(token)
-      .then((files) => {
-        if (!files) return;
-
-        setError(null);
-        setFiles(files);
-      })
-      .catch((error) => setError(error.message));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <DashboardIndex>
@@ -54,19 +34,22 @@ const Dashboard = () => {
               itemsCount={files.length}
               pageSize={PAGE_SIZE}
             />
-            <SmallText
-              style={{
-                textAlign: 'center',
-                maxWidth: '320px',
-                margin: '0 auto',
-                color: COLORS.text4,
-              }}
-            >
-              All your privacy is kept safe at Cloudy. Authentication and
-              encryption are security features that will allow to help secure
-              your data.
-            </SmallText>
           </>
+        )}
+
+        {files?.length > 0 && (
+          <SmallText
+            style={{
+              textAlign: 'center',
+              maxWidth: '320px',
+              margin: '0 auto',
+              color: COLORS.text4,
+            }}
+          >
+            All your privacy is kept safe at Cloudy. Authentication and
+            encryption are security features that will allow to help secure your
+            data.
+          </SmallText>
         )}
       </Section>
     </DashboardIndex>
@@ -75,9 +58,9 @@ const Dashboard = () => {
 
 const Section = styled.section`
   display: grid;
-  gap: 70px;
+  gap: 60px;
   align-content: center;
-  padding: 15px 0px 20px;
+  padding: 20px 0px 20px;
 `;
 
 export default Dashboard;
