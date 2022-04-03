79527cd8886763f4fc872a8727220f7927b8584d
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-04-03 22:45:03 +0530

    Add Lazy loading

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -6,32 +6,42 @@ import {
   Switch,
   useLocation,
 } from 'react-router-dom';
-import FourOhFourPage from '../pages/404Page';
-import AdminLoginPage from '../pages/Admin/AdminLoginPage';
-import DashboardPage from '../pages/Client/Dashboard';
-import LoginPage from '../pages/Client/LoginPage';
-import ProfilePage from '../pages/Client/ProfilePage';
-import SignUp from '../pages/Client/SignUpPage';
-import UploadPage from '../pages/Client/UploadPage';
-import FilesPage from '../pages/Client/FilesPage';
-import HomePage from '../pages/HomePage';
-import DownloadPage from '../pages/Client/DownloadPage';
+import LazyLoading from '../components/Loading/LazyLoading';
+
+const FourOhFourPage = React.lazy(() => import('../pages/404Page'));
+const AdminLoginPage = React.lazy(() =>
+  import('../pages/Admin/AdminLoginPage')
+);
+const DashboardPage = React.lazy(() => import('../pages/Client/Dashboard'));
+const LoginPage = React.lazy(() => import('../pages/Client/LoginPage'));
+const ProfilePage = React.lazy(() => import('../pages/Client/ProfilePage'));
+const SignUp = React.lazy(() => import('../pages/Client/SignUpPage'));
+const UploadPage = React.lazy(() => import('../pages/Client/UploadPage'));
+const FilesPage = React.lazy(() => import('../pages/Client/FilesPage'));
+const HomePage = React.lazy(() => import('../pages/HomePage'));
+const DownloadPage = React.lazy(() => import('../pages/Client/DownloadPage'));
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <ProtectedRoute path="/" exact component={HomePage} />
-        <ProtectedRoute path="/register" exact component={SignUp} />
-        <ProtectedRoute path="/login" exact component={LoginPage} />
-        <ProtectedRoute path="/admin/login" exact component={AdminLoginPage} />
-        <ProtectedRoute path="/dashboard" exact component={DashboardPage} />
-        <ProtectedRoute path="/upload" exact component={UploadPage} />
-        <ProtectedRoute path="/files" exact component={FilesPage} />
-        <ProtectedRoute path="/profile" exact component={ProfilePage} />
-        <Route path="/download/:fileId" exact component={DownloadPage} />
-        <Route path="/*" component={FourOhFourPage} />
-      </Switch>
+      <React.Suspense fallback={<LazyLoading />}>
+        <Switch>
+          <ProtectedRoute path="/" exact component={HomePage} />
+          <ProtectedRoute path="/register" exact component={SignUp} />
+          <ProtectedRoute path="/login" exact component={LoginPage} />
+          <ProtectedRoute
+            path="/admin/login"
+            exact
+            component={AdminLoginPage}
+          />
+          <ProtectedRoute path="/dashboard" exact component={DashboardPage} />
+          <ProtectedRoute path="/upload" exact component={UploadPage} />
+          <ProtectedRoute path="/files" exact component={FilesPage} />
+          <ProtectedRoute path="/profile" exact component={ProfilePage} />
+          <Route path="/download/:fileId" exact component={DownloadPage} />
+          <Route path="*" component={FourOhFourPage} />
+        </Switch>
+      </React.Suspense>
     </Router>
   );
 }
